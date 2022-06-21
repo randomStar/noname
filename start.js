252c512758528b27701f2932bb9c70a1a0674546
M <24471344+randomStar@users.noreply.github.com>
2022-06-21 22:26:16 +0800

    Update start.js

--- start.js
+++ start.js
@@ -1,9 +1,4 @@
 if ("serviceWorker" in navigator) {
-<<<<<<< HEAD
   // register service worker
   navigator.serviceWorker.register("sw.js");
-=======
-    // register service worker
-    navigator.serviceWorker.register("service-worker.js");
->>>>>>> origin/master
 }
